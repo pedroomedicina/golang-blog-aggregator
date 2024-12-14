e2cc1c4e7dfb7c1658eca6e73d04ae980235eb18
Pedro Medina <pedromedina190@gmail.com>
2024-12-14 11:57:20 -0300

    mark feed last fetched datetime

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -20,3 +20,14 @@ SELECT feeds.id, feeds.name, feeds.url, u.name as user_name
 FROM feeds
 INNER JOIN users u ON feeds.user_id = u.id
 WHERE feeds.url = $1;
+
+-- name: MarkFeedFetched :exec
+UPDATE feeds
+SET last_fetched_at = NOW(), updated_at = NOW()
+WHERE id = $1;
+
+-- name: GetNextFeedToFetch :one
+SELECT *
+FROM feeds
+ORDER BY last_fetched_at NULLS FIRST, updated_at
+LIMIT 1;
